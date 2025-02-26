49c2f82d49d7867c903ffce6f3c6d81932cfcd36
Harold Garzon <haroldsneidergarzonaldana53@gmail.com>
2025-02-26 17:42:52 -0500

    se realizan metodos y constructores de las clases #4

--- src/main/java/org/itc/Recurso.java
+++ src/main/java/org/itc/Recurso.java
@@ -6,4 +6,46 @@ abstract class Recurso {
     protected String nombre;
     protected LocalDateTime fechaIngreso;
     protected boolean activo;
+
+    public Recurso(String _nombre,LocalDateTime _fechaIngreso,boolean _activo){
+        nombre = _nombre;
+        fechaIngreso = _fechaIngreso;
+        activo = _activo;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public LocalDateTime getFechaIngreso(){
+        return fechaIngreso;        
+    }
+    
+   
+    public void comprobarEstado(){
+        if (activo) {
+            System.out.println("el estado esta activo");
+        }else{
+            System.out.println("E estado no esta activo");
+        }
+    }
+
+    public void darDeBaja() {
+        if (activo) {
+            activo = false;  // Cambia el estado a inactivo
+            System.out.println("El usuario ha sido dado de baja.");
+        } else {
+            System.out.println("El usuario ya está inactivo.");
+        }
+    } 
+    public boolean coincideConCriterio(String criterio) {
+        // Verifica si el nombre coincide exactamente con el criterio
+        return nombre.equals(criterio);  // Retorna true si coincide, false si no
+    }
+    
+    public String toString(){
+        return "Recurso[activo="+activo+",fechaIngreso="+fechaIngreso+"nombre="+nombre+"] ";
+    }
+
 }
+
