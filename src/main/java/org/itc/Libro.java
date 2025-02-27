f9154827f3c13c0fdc6a10a2b4ef72695662e211
Amayisus01 <diegogerman.amaya@gmail.com>
2025-02-27 16:51:24 -0500

    Actualizacion

--- src/main/java/org/itc/Libro.java
+++ src/main/java/org/itc/Libro.java
@@ -5,29 +5,4 @@ public class Libro extends Recurso {
     private String editorial;
     private int anio;
 
-    public Libro(String _nombre,String _autor,String _editoria,int _anio){
-        nombre =_nombre;
-        autor =_autor;
-        editorial =_editoria;
-        anio = _anio;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public String getEditorial(){
-        return editorial;
-    }
-
-    public int getanio(){
-        return anio;
-    }
-    public boolean coincideConCriterio(String criterio) {
-        // Verifica si el nombre coincide exactamente con el criterio
-        return nombre.equals(criterio);  // Retorna true si coincide, false si no
-    }
-    public String toString(){
-        return "Recurso[activo="+activo+",fechaIngreso="+fechaIngreso+"nombre="+nombre+"] ";
-    }
 }
--- src/main/java/org/itc/Periodico.java
+++ src/main/java/org/itc/Periodico.java
@@ -5,17 +5,4 @@ import java.time.LocalDate;
 public class Periodico extends Recurso{
     private LocalDate fechaPublicacion;
     private String editorial;
-
-    public Periodico(LocalDate _fechaPublicacion,String _editorial){
-        fechaPublicacion = _fechaPublicacion;
-        editorial = _editorial;
-    }
-
-    public boolean coincideConCriterio(String criterio) {
-        // Verifica si el nombre coincide exactamente con el criterio
-        return nombre.equals(criterio);  // Retorna true si coincide, false si no
-    }
-    public String toString(){
-        return "Recurso[activo="+activo+",fechaIngreso="+fechaIngreso+"nombre="+nombre+"] ";
-    }
 }
