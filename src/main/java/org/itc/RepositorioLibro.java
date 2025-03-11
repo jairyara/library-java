02c63e78409662c79f8e4166cdf75ca9e85d4778
Jair Yara <jair.yara11@gmail.com>
2025-03-10 21:11:12 -0500

    chore: Se agrega ejemplo de inyección de clases

---
+++ src/main/java/org/itc/RepositorioLibro.java
@@ -0,0 +1,10 @@
+package org.itc;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RepositorioLibro {
+    public void save(Libro libro) {
+
+    }
+}
--- src/main/java/org/itc/ServicioBiblioteca.java
+++ src/main/java/org/itc/ServicioBiblioteca.java
@@ -1,18 +1,28 @@
 package org.itc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Component
 public class ServicioBiblioteca {
+    private final RepositorioLibro repositorioLibro;
     private List<Recurso> recursos;
 
-    public ServicioBiblioteca() {
+    @Autowired
+    public ServicioBiblioteca(RepositorioLibro repositorioLibro) {
+        this.repositorioLibro = repositorioLibro;
         this.recursos = new ArrayList<>();
     }
 
     public void agregar(Recurso recurso) {
         if (recurso != null) {
             recursos.add(recurso);
+            if (recurso instanceof Libro) {
+                repositorioLibro.save((Libro) recurso);
+            }
         }
     }
 
