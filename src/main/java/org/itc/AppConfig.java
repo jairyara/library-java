4613f87a8509aaf3f6f04c58222263fb14f8fe7a
Jair Yara <jair.yara11@gmail.com>
2025-03-10 20:57:42 -0500

    chore: Se hace actualización del repo a spring framework y se agrega archivo de configuración

---
+++ src/main/java/org/itc/AppConfig.java
@@ -0,0 +1,8 @@
+package org.itc;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.itc")
+public class AppConfig {
+}
